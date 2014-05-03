a3b5e4d0de4dd6b85d89bab720487379a2ac8056
kannibalox <kannibalox@gmail.com>
2014-05-03 04:21:45 -0400

    Add foreground option to manage.py

--- manage.py
+++ manage.py
@@ -147,5 +147,5 @@ if __name__ == "__main__":
             sys.exit(2)
         sys.exit(0)
     else:
-        print "usage: %s start|stop|restart" % sys.argv[0]
+        print "usage: %s start|stop|restart|foreground" % sys.argv[0]
         sys.exit(2)
